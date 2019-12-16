79af7d20707287280299c85c0468c42769b53632
Sedoid <karlsedoide@gmail.com>
2019-12-16 09:42:06 +0100

    added a Loading Text

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
 let spinner = document.getElementsByTagName('span');
 
-const colors=['red','green','orange','blue','pink','purple'];
+const colors=['red','green','orange','blue','brown','purple','violet'];
+const text = 'LOADING'
 var i=0;
 setInterval(function(){
     
   
     
-    if(i> 5)   
+    if(i> 6)   
        {
-        //    spinner[i%6].style.display = 'inline-block';
-           spinner[i%6].style.backgroundColor = '#aaa';
-           spinner[i%6].style.boxShadow = '0 0 0 #fff';
-           spinner[i%6].style.transform = `translateY(-20px)`;
+           spinner[i%7].style.color = '#ccc';
+           spinner[i%7].style.backgroundColor = '#ccc';
+           spinner[i%7].style.boxShadow = '0 0 0 #fff';
+           spinner[i%7].style.transform = `translateY(-2px)`;
     
        } 
     else
        {
            spinner[i].style.backgroundColor =  `${colors[i]}`;
-           spinner[i].style.boxShadow = `0 4px 8px 0 ${colors[i]}, 0 6px 20px 0 ${colors[i]}`;
+           spinner[i].style.boxShadow = `0 4px 8px 0 ${colors[i]}, 0 7px 20px 0 ${colors[i]}`;
            spinner[i].style.transform = `translateY(20px)`;
-        //    spinner[i].style.boxShadow = `0 4px 8px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)`;
+          spinner[i].style.color = `${colors[i]}`;
        } 
         i++; 
-       if(i>12)
+       if(i>13)
        i=0;
    
-}, 500)
+}, 300)
 
-async function wait(i){
-    await setTimeout(function(){
-         
-    }, 200)
-}
